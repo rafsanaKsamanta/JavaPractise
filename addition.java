63ba86ab16bd723cff3926c4b6adb230631883bf
RafsanaKabirSamanta <rafsana@ibos.com>
2024-09-10 16:16:47 +0600

    java practice

---
+++ addition.java
@@ -0,0 +1,13 @@
+
+// 2. Write a Java program to print the sum of two numbers.
+// Test Data:
+// 74 + 36
+// Expected Output :
+// 110
+
+public class addition {
+    public static void main(String args[]) {
+        int sum = 74 + 36;
+        System.out.print(sum);
+    }
+}
---
+++ print.java
@@ -0,0 +1,11 @@
+// 1. Write a Java program to print 'Hello' on screen and your name on a separate
+// line.
+// Expected Output :
+// Hello
+// Alexandra Abramov
+public class print {
+    public static void main(String args[]) {
+        System.out.println("Hello\nAlexandra Abramove");
+
+    }
+}
