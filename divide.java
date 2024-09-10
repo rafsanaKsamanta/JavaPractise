2f24af7057a0ee9821072e58c884b995ac7ea81f
RafsanaKabirSamanta <rafsana@ibos.com>
2024-09-10 17:26:22 +0600

    divide two numbers and print them on the screen

---
+++ divide.java
@@ -0,0 +1,12 @@
+// 3. Write a Java program to divide two numbers and print them on the screen.
+// Test Data :
+// 50/3
+// Expected Output :
+// 16
+
+public class divide {
+    public static void main(String args[]) {
+        int result = 50 / 3;
+        System.out.print(result);
+    }
+}
